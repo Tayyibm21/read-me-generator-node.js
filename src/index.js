cd98986687e04bfc76348945d23f6a15c2a2d935
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2021-11-22 12:20:55 +0000

    boiler plate code and questions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log("Hi");
+// declare questions
+const questions = [
+    {
+        title: "what is the title of your project?",
+   
+      },
+      {
+        title: "what is the description?",
+     
+      },
+      {
+        title: "Do you have an installation script?",
+     
+      },
+      {
+        title: "what is the installation script?",
+ 
+      },
+      {
+        title: "Do you have an application?",
+   
+      },
+      {
+        title: "How do i use the application?",
+ 
+      },
+      
+];
+
+const generateTitle = (answers) => {
+  return '#TITLE ![MIT] (https://img.shields.io/static/v1?label=MIT&message=<MESSAGE>&color=blue)';
+
+};
+
+
+const init = async () => {
+//prompt questions using inquirer
+//generate title using answers
+};
+
+init();
